846ce3af7fcb548fa659c5405abef88468190d8f
Mouni7777 <76986894+Mouni7777@users.noreply.github.com>
2025-06-02 18:25:02 -0400

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Spring Boot is alive";
+    @GetMapping("/hello")
+    public String sendGreetings() {
+        return "Hello, World!";
     }
 }
